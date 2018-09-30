0edcdb52f0d4cacb125a558fad2b9ade4260abab
Kevin W <rocwang0316@gmail.com>
2018-09-29 17:26:45 -0700

    Update the mock component list.

--- app/testHelpers/mockFiles.js
+++ app/testHelpers/mockFiles.js
@@ -4,6 +4,10 @@ jest.mock('react-native-elements', () => ({
   Icon: () => 'Icon',
   Input: () => 'Input',
 }));
+jest.mock('@kevinwang0316/react-native-snackbar-component', () => 'Snackbar');
 
 jest.mock('../components/LoginForm', () => 'LoginForm');
 jest.mock('../components/SocialLoginPanel', () => 'SocialLoginPanel');
+jest.mock('../components/LoginPanel', () => 'LoginPanel');
+jest.mock('../components/RegisterPanel', () => 'RegisterPanel');
+jest.mock('../screens/SafeAreaScreen', () => 'SafeAreaScreen');
