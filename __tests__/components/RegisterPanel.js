89ff16189b35017022617f3a502edc251a148c41
Kevin W <rocwang0316@gmail.com>
2018-09-30 20:39:41 -0700

    Updated some test code.

--- __tests__/components/RegisterPanel.js
+++ __tests__/components/RegisterPanel.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 
-import { RegisterPanel } from '../../app/components/RegisterPanel/RegisterPanel';
+import RegisterPanel from '../../app/components/RegisterPanel/RegisterPanel';
 
 describe('RegisterPanel', () => {
   test('Snapshot', () => expect(renderer.create(<RegisterPanel />).toJSON()).toMatchSnapshot());
