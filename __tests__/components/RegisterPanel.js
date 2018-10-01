7743099c7781969c31286394600e9408ee304d94
Kevin W <rocwang0316@gmail.com>
2018-09-30 17:41:55 -0700

    Added a temporary snapshot test for RegisterPanel to test Travis.

---
+++ __tests__/components/RegisterPanel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import { RegisterPanel } from '../../app/components/RegisterPanel/RegisterPanel';
+
+describe('RegisterPanel', () => {
+  test('Snapshot', () => expect(renderer.create(<RegisterPanel />).toJSON()).toMatchSnapshot());
+});
--- __tests__/screens/LoginScreen.js
+++ __tests__/screens/LoginScreen.js
@@ -12,5 +12,45 @@ jest.mock('../../app/screens/SafeAreaScreen', () => 'SafeAreaScreen');
 describe('LoginScreen', () => {
   const getShallowComponent = () => shallow(<LoginScreen />);
 
+  test('Initial states', () => {
+    const component = getShallowComponent();
+    const {
+      isShowSnackbar, snackbarMessage, snackbarBgColor, isShowRegisterPanel,
+    } = component.state();
+
+    expect(isShowSnackbar).toBe(false);
+    expect(isShowRegisterPanel).toBe(false);
+    expect(snackbarMessage).toBe('');
+    expect(snackbarBgColor).toBe('');
+  });
+
+  test('snackbarUpdateCallback', () => {
+    jest.useFakeTimers();
+    const component = getShallowComponent();
+    component.instance().snackbarUpdateCallback({ isShowSnackbar: true, snackbarMessage: 'message', snackbarBgColor: 'bgColor' });
+
+    expect(component.state('isShowSnackbar')).toBe(true);
+    expect(component.state('snackbarMessage')).toBe('message');
+    expect(component.state('snackbarBgColor')).toBe('bgColor');
+
+    jest.runAllTimers();
+    expect(component.state('isShowSnackbar')).toBe(false);
+  });
+
+  test('handlePanelSwitch', () => {
+    const component = getShallowComponent();
+    expect(component.state('isShowRegisterPanel')).toBe(false);
+    component.instance().handlePanelSwitch();
+    expect(component.state('isShowRegisterPanel')).toBe(true);
+  });
+
   test('Snapshot with isShowRegisterPanel false', () => expect(renderer.create(<LoginScreen />).toJSON()).toMatchSnapshot());
+  test('Snapshot with isShowRegisterPanel true ans isShowSnackbar true', () => {
+    const component = getShallowComponent();
+    component.setState({
+      isShowSnackbar: true,
+      isShowRegisterPanel: true,
+    });
+    expect(renderer.create(component).toJSON()).toMatchSnapshot();
+  });
 });
