fe56b4bb5115ce1d4074cd42b69cf3187c6e115c
Kevin W <rocwang0316@gmail.com>
2018-10-05 17:18:09 -0700

    Added the UserInfoScreen component and test.

---
+++ __tests__/screens/UserInfoScreen.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import { UserInfoScreen } from '../../app/screens/UserInfoScreen';
+
+describe('UserInfoScreen', () => {
+  const defaultProps = {
+    user: {
+      displayName: 'displayName',
+      avatar: 'avatarUrl',
+    },
+    navigation: {
+      goBack: jest.fn(),
+    },
+    logout: jest.fn(),
+  };
+  const getShallowComponent = (props = defaultProps) => shallow(<UserInfoScreen {...props} />);
+
+  test('click closeBtn', () => {
+    const component = getShallowComponent();
+    component.find('Button').at(1).simulate('press');
+
+    expect(defaultProps.navigation.goBack).toHaveBeenCalledTimes(1);
+  });
+
+  test('click logoutBtn', () => {
+    const component = getShallowComponent();
+    component.find('Button').at(0).simulate('press');
+
+    expect(defaultProps.logout).toHaveBeenCalledTimes(1);
+    expect(defaultProps.navigation.goBack).toHaveBeenCalledTimes(2);
+  });
+
+  test('Snapshot', () => expect(renderer.create(<UserInfoScreen {...defaultProps} />).toJSON()).toMatchSnapshot());
+});
